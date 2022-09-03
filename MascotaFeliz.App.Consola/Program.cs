5d47dbb1692dd65cd270e935cc1384fbc815567f
CRIVGOM <cristianriverag99@gmail.com>
2022-09-03 13:10:27 -0500

    Proyecto

---
+++ MascotaFeliz.App.Consola/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia;
+
+namespace MascotaFeliz.App.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        
+    }
+}
