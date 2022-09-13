6316ecbe7a0a3abe233f90babac2e9056d065076
sindulfoh <sinherani@gmail.com>
2022-09-13 13:06:21 -0500

    repositorio de veterinario

--- MascotaFeliz.App.Consola/Program.cs
+++ MascotaFeliz.App.Consola/Program.cs
@@ -6,11 +6,40 @@ namespace MascotaFeliz.App.Consola
 {
     class Program
     {
+        private static IRepositorioVeterinario _repoVeterinario = new RepositorioVeterinario(new Persistencia.AppContext());
+        private static IRepositorioDueno _repoDueno = new RepositorioDueno(new Persistencia.AppContext());
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            AddDueno();
+            AddVeterinario();
         }
 
-        
+        private static void AddDueno()
+        {
+            var dueno = new Dueno
+            {
+                //Cedula = "1212",
+                Nombres = "miguel",
+                Apellidos = "san juan",
+                Direccion = "san juan",
+                Telefono = "123456891",
+                Correo = "elquees@gmail.com"
+            };
+            _repoDueno.AddDueno(dueno);
+        }
+        private static void AddVeterinario()
+        {
+            var veterinario = new Veterinario
+            {
+                //Cedula = "1212",
+                Nombres = "Juan carlos",
+                Apellidos = "martinez",
+                Direccion = "escallon vills",
+                Telefono = "3124586576",
+                TarjetaProfesional = "1256645635"
+            };
+            _repoVeterinario.AddVeterinario(veterinario);
+        }
     }
 }
