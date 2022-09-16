91936dac3962ebfc5f3025652e1712a762af1a74
sinusername666 <narvaezmr90@gmail.com>
2022-09-16 10:06:48 -0500

    Repositorio Historia

--- MascotaFeliz.App.Consola/Program.cs
+++ MascotaFeliz.App.Consola/Program.cs
@@ -6,11 +6,23 @@ namespace MascotaFeliz.App.Consola
 {
     class Program
     {
+        private static IRepositorioHistoria _repoHistoria = new RepositorioHistoria(new Persistencia.AppContext());
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            AddHistoria();
         }
 
-        
+        private static void AddHistoria()
+        {
+            var historia = new Historia
+            {
+                FechaInicial = new DateTime(2020, 01, 01),
+                
+            };
+            _repoHistoria.AddHistoria(historia);
+        }
     }
 }
